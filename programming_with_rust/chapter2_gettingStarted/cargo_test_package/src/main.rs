2f7b8b920df6bf23f5f0ebe554caa3a68139c997
Uras Ayanoglu <uras@ayanoglu.org>
2024-04-19 15:00:04 +0300

    Add package structure and the main source file created by cargo

---
+++ programming_with_rust/chapter2_gettingStarted/cargo_test_package/src/main.rs
@@ -0,0 +1,13 @@
+/*
+File      :   main.rs
+Time      :   19.04.2024 14:05:52
+Compile   :   rustc -C debuginfo=2 -W warnings main.rs -o main
+Version   :   1.0
+Author(s) :   Uras Ayanoglu
+Contact   :   uras.ayanoglu@edu.turkuamk.fi
+Desc      :   A basic cargo test package created with cargo new cargo_test_package
+*/
+
+fn main() {
+    println!("Hello, world!"); // prints Hello, world! to the console
+}
