1953a7e0dd24ae22078177c6571093d86c7051cc
Uras Ayanoglu <uras@ayanoglu.org>
2024-04-19 22:34:11 +0300

    Add hello world example with cargo and rustc

---
+++ rust_programming_language/chapter1_gettings_started/hello_world.rs
@@ -0,0 +1,13 @@
+/*
+File      :   hello_world.rs
+Time      :   19.04.2024 16:02:36
+Compile   :   rustc -C debuginfo=2 -W warnings hello_world.rs -o hello_world
+Version   :   1.0
+Author(s) :   Uras Ayanoglu
+Contact   :   uras.ayanoglu@edu.turkuamk.fi
+Desc      :   None
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
