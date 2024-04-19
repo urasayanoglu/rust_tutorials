7947336134de7dd0c5c0913e191c80b79620758a
Uras Ayanoglu <uras@ayanoglu.org>
2024-04-19 11:55:53 +0300

    Add a basic program that prints Hello World

---
+++ programming_with_rust/chapter2_gettingStarted/helloWorld.rs
@@ -0,0 +1,13 @@
+/*
+File      :   helloWorld.rs
+Time      :   19.04.2024 11:39:26
+Compile   :   rustc -C debuginfo=2 -W warnings helloWorld.rs -o helloWorld
+Version   :   1.0
+Author(s) :   Uras Ayanoglu
+Contact   :   uras.ayanoglu@edu.turkuamk.fi
+Desc      :   A basic hello world program in Rust.
+*/
+
+fn main(){
+    println!("Hello World!");
+}
